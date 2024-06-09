d6a5ffaa0d99d9e99a70a95358f8af303adabdcf
Natiene <cardosonatiene@gmail.com>
2024-06-09 18:30:38 -0300

    create confirm password service tests

--- src/app/shared/services/confirmPassword/consfirm-password.service.spec.ts
+++ src/app/shared/services/confirmPassword/consfirm-password.service.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ConfirmPasswordService } from './consfirm-password.service';
 
-import { ConsfirmPasswordService } from './consfirm-password.service';
-
-describe('ConsfirmPasswordService', () => {
-  let service: ConsfirmPasswordService;
+describe('ConfirmPasswordService', () => {
+  let service: ConfirmPasswordService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ConsfirmPasswordService);
+    service = TestBed.inject(ConfirmPasswordService);
   });
 
-  it('should be created', () => {
+  it('should create', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should validate that passwords match', () => {
+    const formGroup = new FormGroup({
+      password: new FormControl('password123'),
+      confirm_password: new FormControl('password123')
+    });
+
+    const validationResult = ConfirmPasswordService.MatchingPassword(formGroup);
+    expect(validationResult).toBeNull();
+    expect(formGroup.get('confirm_password')?.errors).toBeNull();
+  });
+
+  it('should validate that passwords do not match', () => {
+    const formGroup = new FormGroup({
+      password: new FormControl('password123'),
+      confirm_password: new FormControl('differentpassword')
+    });
+
+    const validationResult = ConfirmPasswordService.MatchingPassword(formGroup);
+    expect(validationResult).toBeNull();
+    expect(formGroup.get('confirm_password')?.errors).toEqual({ not_matching: true });
+  });
+
+  it('should validate that passwords do not match and confirm password is empty', () => {
+    const formGroup = new FormGroup({
+      password: new FormControl('password123'),
+      confirm_password: new FormControl('')
+    });
+
+    const validationResult = ConfirmPasswordService.MatchingPassword(formGroup);
+    expect(validationResult).toBeNull();
+    expect(formGroup.get('confirm_password')?.errors).toBeNull();
+  });
+
+  it('should handle when controls are not found', () => {
+    const formGroup = new FormGroup({
+      password: new FormControl('password123'),
+    });
+
+    const validationResult = ConfirmPasswordService.MatchingPassword(formGroup);
+    expect(validationResult).toBeNull();
+    expect(formGroup.get('confirm_password')).toBeNull();
   });
 });
