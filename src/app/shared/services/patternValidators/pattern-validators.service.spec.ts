0908c4a9e311936f4f9151366f9a67565ecdbb80
Natiene <cardosonatiene@gmail.com>
2024-06-09 18:40:44 -0300

    create pattern validators tests

--- src/app/shared/services/patternValidators/pattern-validators.service.spec.ts
+++ src/app/shared/services/patternValidators/pattern-validators.service.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-
+import { FormControl, ValidationErrors } from '@angular/forms';
 import { PatternValidatorsService } from './pattern-validators.service';
 
 describe('PatternValidatorsService', () => {
-  let service: PatternValidatorsService;
+  let patternValidatorsService: PatternValidatorsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(PatternValidatorsService);
+    patternValidatorsService = TestBed.inject(PatternValidatorsService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(patternValidatorsService).toBeTruthy();
+  });
+
+  it('should return null if control is empty', () => {
+    const validatorFn = PatternValidatorsService.patternValidators(/\d/, { hasNumber: true });
+    const control = new FormControl('');
+    const validationResult = validatorFn(control);
+
+    expect(validationResult).toBeNull();
+  });
+
+  it('should return null if value matches the regex', () => {
+    const validatorFn = PatternValidatorsService.patternValidators(/\d/, { hasNumber: true });
+    const control = new FormControl('1234');
+    const validationResult = validatorFn(control);
+
+    expect(validationResult).toBeNull();
+  });
+
+  it('should return error if value does not match the regex', () => {
+    const validatorFn = PatternValidatorsService.patternValidators(/\d/, { hasNumber: true });
+    const control = new FormControl('abcd');
+    const validationResult = validatorFn(control);
+
+    expect(validationResult).toEqual({ hasNumber: true });
+  });
+
+  it('should return null if value matches a complex regex', () => {
+    const validatorFn = PatternValidatorsService.patternValidators(/^(?=.*[A-Z])(?=.*\d).+$/, { complexPattern: true });
+    const control = new FormControl('Password1$');
+    const validationResult = validatorFn(control);
+
+    expect(validationResult).toBeNull();
+  });
+
+  it('should return error if value does not match a complex regex', () => {
+    const validatorFn = PatternValidatorsService.patternValidators(/^(?=.*[A-Z])(?=.*\d).+$/, { complexPattern: true });
+    const control = new FormControl('password');
+    const validationResult = validatorFn(control);
+
+    expect(validationResult).toEqual({ complexPattern: true });
   });
 });
